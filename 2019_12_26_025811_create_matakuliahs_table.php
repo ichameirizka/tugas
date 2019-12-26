db2e9e8be9a5c7df2a2949955144a21085bb2960
ichameirizka <59228315+ichameirizka@users.noreply.github.com>
2019-12-26 10:54:29 +0700

    Update and rename 2019_12_23_031709_create_mata_kuliahs_table.php to 2019_12_26_025811_create_matakuliahs_table.php

---
+++ 2019_12_26_025811_create_matakuliahs_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMatakuliahsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('matakuliahs', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->char('kd_matakuliah',9) -> unique();
+            $table->string('nama_matakuliah',50);
+            $table->char('jumlah_SKS',6);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('matakuliahs');
+    }
+}
